e4df8d0aaa2726a270eae2d0b5aa73b234b5d20e
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-02-02 21:40:04 +0500

    feat: add lead component

---
+++ src/components/Lead/Lead.tsx
@@ -0,0 +1,10 @@
+import styles from "./Lead.module.css";
+
+export default function Lead() {
+  return (
+    <section className={styles.lead}>
+      <h2>AUTO REPAIR AND COLLISION CENTER</h2>
+      <p>Reliable and knowledgeable mechanics located in Los Angeles, CA</p>
+    </section>
+  );
+}
