e0a221e0d88367d08138f96060e571a78db4c01c
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-02-02 22:31:55 +0500

    feat: add article component

--- src/components/Lead/Lead.tsx
+++ src/components/Lead/Lead.tsx
@@ -1,10 +1,10 @@
+import SectionHeader from "../SectionHeader/SectionHeader";
 import styles from "./Lead.module.css";
 
 export default function Lead() {
   return (
     <section className={styles.lead}>
-      <h2>AUTO REPAIR AND COLLISION CENTER</h2>
-      <p>Reliable and knowledgeable mechanics located in Los Angeles, CA</p>
+      <div className={styles.info}><SectionHeader /></div>
     </section>
   );
 }
---
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -0,0 +1,10 @@
+import styles from './SectionHeader.module.css';
+
+export default function SectionHeader() {
+  return (
+    <>
+      <h2 className={styles.title}>AUTO REPAIR AND COLLISION CENTER</h2>
+      <p className={styles.text}>Reliable and knowledgeable mechanics located in Los Angeles, CA</p>
+    </>
+  );
+}
