850fa6ead2dba4e7d66c30128202c79cdcbfa112
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-02-03 09:44:58 +0500

    feat: add data file with headers typography

---
+++ src/components/Service/Service.tsx
@@ -0,0 +1,10 @@
+import SectionHeader from "../SectionHeader/SectionHeader";
+import "./Service.module.css";
+
+export default function Service() {
+  return (
+    <section>
+      <SectionHeader />
+    </section>
+  );
+}
---
+++ src/data/typography.tsx
@@ -0,0 +1,34 @@
+export const sectionHeaders = {
+  lead: {
+    title: "AUTO REPAIR AND COLLISION CENTER",
+    text: "Reliable and knowledgeable mechanics located in Los Angeles, CA",
+  },
+  service: {
+    title: "We keep your car on the road with professional prompt service",
+    text: "We are dedicated to the highest form of customer service and quality delivered with a sense of warmth and friendliness.",
+  },
+  experience: {
+    title: "Twenty Years of Combined Experience",
+    text: "Yourik and Artin Reyhanian have run the shop for the last 10 years. The brothers pride themselves on reliable honest auto care and body work for domestic and foreign cars.",
+  },
+  guarantees: {
+    title:
+      "We offer an extensive range of car repair & maintenance services for domestic and foreign cars",
+    text: "FIXED RIGHT THE FIRST TIME. GUARANTEED",
+  },
+  professionalism: {
+    title: "A professional automotive repair and auto maintenance company",
+    text: "M Fast Auto has been providing quality car care in Los Angeles since 2002. We are a family-owned business delivering honest and professional automotive repair and auto maintenance services to the people of Los Angeles and surrounding areas.",
+  },
+  knownFor: {
+    title:
+      "We are known for our customer service and for offering the best products and services",
+  },
+  offer: {
+    title: "Below are some of the repair services that we offer",
+  },
+  contact: {
+    title:
+      "For any additional questions regarding M Fast Auto and our our services, feel free to contact us",
+  },
+};
