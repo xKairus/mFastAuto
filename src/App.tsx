f6e7a207c11e7aa3faefa83bdb5fdc77b4f9ae98
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-03-08 16:44:12 +0500

    dev: add responsive styles for Lead component

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ function App() {
   return (
     <>
       <Header />
-      {/* <Lead />
-      <Service />
+      <Lead />
+      {/* <Service />
       <Experience />
       <Guarantees />
       <Professionalism />
