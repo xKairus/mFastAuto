230d969e0c6eaa6cf06565e553268424e9472cc2
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-03-21 18:06:27 +0500

    fix: solve animation problem

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -2,20 +2,20 @@ import React from "react"
 
 export const fadeIn = (isAnimated: boolean): React.CSSProperties => ({
   opacity: isAnimated ? 1 : 0,
-  transition: "opacity 0,8s ease-in-out",
+  transition: "opacity 0.8s ease-in-out",
 })
 
 export const moveRight = (isAnimated: boolean): React.CSSProperties => ({
   transform: isAnimated ? "translateX(0)" : "translateX(-100px)",
-  transition: "transform 0,8s ease-in-out",
+  transition: "transform 0.8s ease-in-out",
 })
 
 export const moveLeft = (isAnimated: boolean): React.CSSProperties => ({
   transform: isAnimated ? "translateX(0)" : "translateX(100px)",
-  transition: "transform 0,8s ease-in-out",
+  transition: "transform 0.8s ease-in-out",
 })
 
 export const scaleUp = (isAnimated: boolean): React.CSSProperties => ({
   transform: isAnimated ? "scale(1)" : "scale(0.6)",
-  transition: "transform 0,8s ease-in-out",
+  transition: "transform 0.8s ease-in-out",
 })
